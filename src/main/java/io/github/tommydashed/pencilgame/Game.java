c0510f9145b6e7f880d5f04869edbb2fe4a13ab9
Tom-Sawyer-123 <yungf4st@gmail.com>
2026-02-16 19:15:15 -0800

    add user input for pencil count

--- src/main/java/io/github/tommydashed/pencilgame/Game.java
+++ src/main/java/io/github/tommydashed/pencilgame/Game.java
@@ -1,18 +1,28 @@
 package io.github.tommydashed.pencilgame;
+import java.util.Scanner;
 
 public class Game {
 
+    private final Scanner scanner = new Scanner(System.in);
+
     public void run() {
-        int pencils = 5;
-        String first = "Jacob";
+        int pencils = readPencilCount(); //get pencil count and store value in var
+        String first = "Jacob"; // store placeholder name for now
+
+        // print pencils, will extract to its own method later
         for (int i = 0; i < pencils; i++) {
             System.out.print("|");
 
         }
+
+        // print a new line then print line with string
         System.out.println();
         System.out.println(first + " is going first!");
     }
-}
-
-
-
+        // read the pencil count
+    private int readPencilCount() {
+        System.out.println("How many pencils would you like to use:");
+        String line = scanner.nextLine().trim();
+        return Integer.parseInt(line);
+        }
+    }
--- src/main/java/io/github/tommydashed/pencilgame/Main.java
+++ src/main/java/io/github/tommydashed/pencilgame/Main.java
@@ -2,6 +2,7 @@ package io.github.tommydashed.pencilgame;
 
 public class Main {
     public static void main(String[] args) {
+        // run game
         new Game().run();
     }
 }

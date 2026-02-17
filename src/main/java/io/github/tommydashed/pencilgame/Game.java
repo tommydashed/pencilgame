7950a83db9d52e2a0638ca6e934d44478a8f0e0a
Tom-Sawyer-123 <yungf4st@gmail.com>
2026-02-16 18:31:01 -0800

    render initial pencils and first player message

--- src/main/java/io/github/tommydashed/pencilgame/Game.java
+++ src/main/java/io/github/tommydashed/pencilgame/Game.java
@@ -1,7 +1,18 @@
 package io.github.tommydashed.pencilgame;
 
 public class Game {
-    public void run() {
 
+    public void run() {
+        int pencils = 5;
+        String first = "Jacob";
+        for (int i = 0; i < pencils; i++) {
+            System.out.print("|");
+
+        }
+        System.out.println();
+        System.out.println(first + " is going first!");
     }
 }
+
+
+

9139504970560ba136a0149a9b9483a9d31e1b1c
Tom-Sawyer-123 <yungf4st@gmail.com>
2026-02-16 19:37:36 -0800

    add user input for first player selection

--- src/main/java/io/github/tommydashed/pencilgame/Game.java
+++ src/main/java/io/github/tommydashed/pencilgame/Game.java
@@ -6,8 +6,8 @@ public class Game {
     private final Scanner scanner = new Scanner(System.in);
 
     public void run() {
-        int pencils = readPencilCount(); //get pencil count and store value in var
-        String first = "Jacob"; // store placeholder name for now
+        int pencils = readPencilCount(); //get pencil count input and store value in var
+        String first = readPlayerName(); // get player name input and store in var
 
         // print pencils, will extract to its own method later
         for (int i = 0; i < pencils; i++) {
@@ -19,10 +19,16 @@ public class Game {
         System.out.println();
         System.out.println(first + " is going first!");
     }
-        // read the pencil count
+
+    // read the pencil count
     private int readPencilCount() {
         System.out.println("How many pencils would you like to use:");
         String line = scanner.nextLine().trim();
         return Integer.parseInt(line);
-        }
     }
+
+    private String readPlayerName() {
+        System.out.println("Who will be first player:");
+        return scanner.nextLine().trim();
+    }
+}
